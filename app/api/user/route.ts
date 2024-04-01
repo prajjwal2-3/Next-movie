9c1e08d4a831df72d7e6fd20aaa36bfa654a4093
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-04-01 17:37:31 +0530

    initial

---
+++ app/api/user/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function GET(req: NextRequest) {
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/authentication',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGRkMDlkZWM0ZDczNjM2Njk2Nzk3ODFhMWVlMDBjOCIsInN1YiI6IjY1ZDYzNzYyOTk3NGVlMDE3YjA1NjIzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OJRc_SaCQ-2Fmm4GrUs45cU7un1Vqt55vy4ttrrV42U'
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
